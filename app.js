fa74f4d455ccc21340e9f3202a87d3c954aa445d
bRRAAz <jvtomedi@gmail.com>
2022-07-22 23:47:22 -0300

    Finalização do Projeto

--- app.js
+++ app.js
@@ -1,38 +1,43 @@
+//Inputs e Variáveis
 let p_total = document.querySelector(".p_total")
 let p_amount = document.querySelector(".p_amount")
 let dolarinput = document.querySelector("#dolar_inp")
 let peaple_input = document.querySelector("#peaple_inp")
 let custom_input = document.querySelector(".custom")
 let radiobtn = document.querySelectorAll(".radiobtn")
+//Função para resetar o splicer
 function reset() {
+    radio = document.querySelector(".radiobtn[name='tip']:checked")
+    radio.checked = false
     p_total.innerHTML = "0,00"
     p_amount.innerHTML = "0,00"
     dolarinput.value = ""
     peaple_input.value = ""
     custom_input.value = ""
 }
+//Chamada da função principal
+dolarinput.addEventListener('input', call)
+peaple_input.addEventListener('input', call)
 
-dolarinput.addEventListener('input',call)
-peaple_input.addEventListener('input',call)
-
-radiobtn.forEach(function(val){
-    val.addEventListener('click',call)
+radiobtn.forEach(function (val) {
+    val.addEventListener('click', call)
 })
-custom_input.addEventListener('input',call)
+custom_input.addEventListener('input', call)
 
+//Função dos calculos
+function total(inp) {
+    let percent = dolarinput.value * (inp.value / 100)
+    let total_per_person = (Number(dolarinput.value) + Number(percent)) / peaple_input.value
+    p_total.innerHTML = `${total_per_person.toFixed(2).replace(".", ",")}`
+    p_amount.innerHTML = `${(percent / peaple_input.value).toFixed(2).replace(".", ",")}`
+}
+//Autenticador/Função principal
 function call() {
     radio = document.querySelector(".radiobtn[name='tip']:checked")
-    
-    if(custom_input.value != "" && dolarinput.value > 1 && peaple_input.value > 1){
-        let percent = dolarinput.value * (custom_input.value / 100)
-        let total_per_person = (Number(dolarinput.value) + Number(percent)) / peaple_input.value
-        p_total.innerHTML = `${total_per_person.toFixed(2).replace(".",",")}`
-        p_amount.innerHTML = `${(percent/peaple_input.value).toFixed(2).replace(".",",")}`
-    }else if (dolarinput.value > 1 && peaple_input.value > 1 && radio.value > 1) {
-        let percent = dolarinput.value * (radio.value / 100)
-        let total_per_person = (Number(dolarinput.value) + Number(percent)) / peaple_input.value
-        p_total.innerHTML = `${total_per_person.toFixed(2).replace(".",",")}`
-        p_amount.innerHTML = `${(percent/peaple_input.value).toFixed(2).replace(".",",")}`
+    if (custom_input.value != "" && dolarinput.value > 1 && peaple_input.value > 1) {
+        total(custom_input)
+    } else if (dolarinput.value > 1 && peaple_input.value > 1 && radio.value > 1) {
+        total(radio)
     }
 }
 
